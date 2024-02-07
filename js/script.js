5ad68a850d204a4c37cb818a5be61864f234e23d
Joao Silva <120136350+jcizidrosilva@users.noreply.github.com>
2024-02-07 00:04:25 -0300

    criptografando a mensagem

--- js/script.js
+++ js/script.js
@@ -20,16 +20,90 @@ function removerAcentos(texto) {
     .replace(/[ñ]/g, "n");
 }
 
+// Craindo a Criptografia usando um array
+const substituicoes = [
+  { original: "e", substituto: "enter" },
+  { original: "i", substituto: "imes" },
+  { original: "a", substituto: "ai" },
+  { original: "o", substituto: "ober" },
+  { original: "u", substituto: "ufat" },
+];
+
+// Ocultando o botao copiar
+asideBtnCopiar.style.display = "none";
+
+// Adicionando um ouvinte de evento de clique ao botão Criptografar
 btnCriptografar.addEventListener("click", function () {
-  // Obtendo o resultado do texto digitado no header textarea
+  // Obtendo o texto digitado pelo usuário no headerTextArea
   let textoCriptografar = headerTextArea.value;
 
-  // Removendo acentos e convertendo para letras minusculas somente
+  // Removendo acentos e convertendo para letras minúsculas somente
   textoCriptografar = removerAcentos(textoCriptografar).toLowerCase();
 
-  // resultado do texto do header textarea salvo na variavel textoCriptografar
-  console.log("Texto a ser criptografado:", textoCriptografar);
+  // Aplicando as substituições com base no array substituicoes
+  substituicoes.forEach((substituicao) => {
+    textoCriptografar = textoCriptografar.replace(
+      new RegExp(substituicao.original, "g"),
+      substituicao.substituto
+    );
+  });
 
-  // Exibindo resultado do texto fornecido pelo usuario no header textarea agora sendo exibido no aside textarea
+  // Exibindo o resultado do texto criptografado no asideTextArea
   asideTextArea.value = textoCriptografar;
+
+  // Limpando o texto digitado no headerTextArea
+  headerTextArea.value = "";
+
+  // Exibindo o botão Copiar
+  asideBtnCopiar.style.display = "block";
 });
+
+// Adicionando funcionalidade ao botao copiar
+asideBtnCopiar.addEventListener("click", function () {
+  // selecionando o texto no asideTextArea
+  asideTextArea.select();
+
+  // Copiar o texto selecionado
+  document.execCommand("copy");
+
+  // Agora vamos remover a mensagem anterior
+  asideTextArea.value = "";
+
+  // Removendo foco do asideTextArea
+  asideTextArea.blur();
+
+  // Removendo mensagem anterior e adicionando uma nova mensagem
+  asideTextArea.value = "";
+  asideTextArea.placeholder = "Texto copiado com sucesso! Cole onde desejar.";
+  asideTextArea.style.fontSize = "2rem";
+  asideTextArea.style.color = "#0a3871";
+});
+
+//
+//
+//
+//
+// codigo que nao funcionou
+// // Adicionando um evento ao botao criptografar
+// btnCriptografar.addEventListener("click", function () {
+//   // Obtendo o texto digitado pelo usuario
+//   let texto = headerTextArea.value;
+
+//   // Agora vamos percorrer o texto aplicando a criptografia
+//   substituicoes.forEach((substituicao) => {
+//     texto = texto.replace(
+//       new RegExp(substituicao.original, "g"),
+//       substituicao.substituto
+//     );
+//   });
+
+//   // Agora vamos exibir a criptografia
+//   asideTextArea.value = texto;
+
+//   // Agora vamos limpar o texto digitado pelo usuario no headerTextArea
+//   headerTextArea.value = "";
+// });
+
+//
+//
+//
---
+++ texto.md
@@ -0,0 +1,13 @@
+const substituicoes = [
+{ original: "e", substituto: "enter" },
+{ original: "i", substituto: "imes" },
+{ original: "a", substituto: "ai" },
+{ original: "o", substituto: "ober" },
+{ original: "u", substituto: "ufat" },
+];
+
+primeira frase
+
+meu nome e joao
+
+menterufat nobermenter enter joberaiober
